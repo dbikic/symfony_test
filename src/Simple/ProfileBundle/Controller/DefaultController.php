13657b0f074cff5c1c33d9c272c16ce5d114d37c
dino <bikic.dino@gmail.com>
2014-09-05 20:11:23 +0200

    drugi commit

--- src/Simple/ProfileBundle/Controller/DefaultController.php
+++ src/Simple/ProfileBundle/Controller/DefaultController.php
@@ -3,11 +3,36 @@
 namespace Simple\ProfileBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Simple\ProfileBundle\Entity\Osoba;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('SimpleProfileBundle:Default:index.html.twig', array('name' => $name));
+		
+        return $this->render('SimpleProfileBundle:Default:index.html.twig', array());
     }
+	
+	public function createAction()
+	{
+		$osoba = new Osoba();
+		$osoba->setName('Dino');
+		$osoba->setSurname('Bikic');
+		$osoba->setEmail('asd@asd.asd');
+		$osoba->setPhone('789456');
+		$osoba->setAddress('asd 12, AD2');
+		
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($osoba);
+		$em->flush();
+		
+		return new Response('Created osoba sa id '.$osoba->getId());
+	}
+	
+	public function showAction(){
+		$osoba = $this->getDoctrine()
+			->getRepository('SimpleProfileBundle:Osoba')
+			->findAll();
+	}
 }

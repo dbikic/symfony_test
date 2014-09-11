3d4644ce2a6758e4a9bd81016b7834e330990a69
dino <bikic.dino@gmail.com>
2014-09-11 22:21:28 +0200

    update i delete

--- src/Simple/ProfileBundle/Controller/OsobaController.php
+++ src/Simple/ProfileBundle/Controller/OsobaController.php
@@ -86,4 +86,72 @@ class OsobaController extends Controller
             )
         );
     }
+	public function editAction($id)
+    {
+		$osoba = $this->getDoctrine()
+			->getRepository('SimpleProfileBundle:Osoba')
+			->find($id);		
+		if (!$osoba) {
+            throw $this->createNotFoundException('Ne postoji osoba pod ovim id-om!');
+        }
+		$form = $this->createForm(new OsobaType(), $osoba);
+		return $this->render(
+            'SimpleProfileBundle:Osoba:edit.html.twig',
+            array(
+                'osoba' => $osoba,
+                'update_form' => $form->createView(),
+            )
+        );
+	}
+	
+	public function updateAction(Request $request, $id)
+    {	
+	
+		$osoba = $this->getDoctrine()
+			->getRepository('SimpleProfileBundle:Osoba')
+			->find($id);
+		if (!$osoba) {
+            throw $this->createNotFoundException('Ne postoji osoba pod ovim id-om!');
+        }
+		
+        $form = $this->createForm(new OsobaType(), $osoba);
+        $form->bind($request);
+		
+	    $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->flush();
+	
+		if ($form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirect($this->generateUrl('profil_pregled', array('id' => $osoba->getId())));			
+        }
+	
+		return $this->render(
+            'SimpleProfileBundle:Osoba:new.html.twig',
+            array(
+                'osoba' => $osoba,
+                'form' => $form->createView(),
+            )
+        );
+		
+    }
+	public function deleteAction($id)
+    {	
+	
+		$osoba = $this->getDoctrine()
+			->getRepository('SimpleProfileBundle:Osoba')
+			->find($id);
+		
+		if (!$osoba) {
+            throw $this->createNotFoundException('Ne postoji osoba pod ovim id-om!');
+        }
+		
+	    $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($osoba);
+        $entityManager->flush();
+	
+		return $this->redirect($this->generateUrl('profil_ispis'));			
+		
+    }
 }
